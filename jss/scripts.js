a9246ff3c6f07252b031fe152313142976574a70
peter302 <petermbaik@gmail.com>
2018-07-03 10:55:34 +0300

    added jcss,html and jss folder and j.css,j.html and scripts.js

---
+++ jss/scripts.js
@@ -0,0 +1,10 @@
+
+  var add = function(number1, number2) {
+    return number1 + number2;
+  }
+var number1 = parseInt(prompt("please insert a number :"));
+alert(number1);
+var number2 = parseInt(prompt("please input the other number:"));
+alert(number2);
+var result = add(number1, number2);
+alert(result);
